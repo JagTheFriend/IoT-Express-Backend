40dbf8c554cd0cf1a818f48eb169471d107227b8
JagTheFriend <JagTheFriend12@gmail.com>
2025-02-01 13:43:26 +0530

    feat: Setup simple server
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/user/new", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
